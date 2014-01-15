dca2a5e194406fd846bb995548b94b64cdc1ac80
Yuto Takei <takeiyuto@outlook.com>
2014-01-15 23:15:29 +0900

    Initial commit

---
+++ Location/YTOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YTOAppDelegate.h
+//  Location
+//
+//  Created by Yuto Takei on 1/15/14.
+//  Copyright (c) 2014 Yuto Takei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YTOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Location/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Location
+//
+//  Created by Yuto Takei on 1/15/14.
+//  Copyright (c) 2014 Yuto Takei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "YTOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YTOAppDelegate class]));
+    }
+}
