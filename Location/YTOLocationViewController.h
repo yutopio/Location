d48aa5fa4f7a2f39dd1d7f89e26deb4c11794212
Yuto Takei <takeiyuto@outlook.com>
2014-01-16 11:20:58 +0900

    Enabled location retrieval

---
+++ Location/YTOLocationViewController.h
@@ -0,0 +1,14 @@
+//
+//  YTOLocationViewController.h
+//  Location
+//
+//  Created by Yuto Takei on 1/16/14.
+//  Copyright (c) 2014 Yuto Takei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface YTOLocationViewController : UIViewController < CLLocationManagerDelegate >
+
+@end
---
+++ Location/YTOLocationViewController.m
@@ -0,0 +1,69 @@
+//
+//  YTOLocationViewController.m
+//  Location
+//
+//  Created by Yuto Takei on 1/16/14.
+//  Copyright (c) 2014 Yuto Takei. All rights reserved.
+//
+
+#import "YTOLocationViewController.h"
+#import <CoreLocation/CoreLocation.h>
+
+@interface YTOLocationViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *latitude;
+@property (weak, nonatomic) IBOutlet UITextField *longitude;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator1;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indicator2;
+
+@end
+
+@implementation YTOLocationViewController
+
+CLLocationManager *lm;
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self updateLocation];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    if (!lm) {
+        [lm stopUpdatingLocation];
+    }
+}
+
+- (IBAction)updateLocation
+{
+    if (!lm) {
+        lm = [[CLLocationManager alloc] init];
+    }
+
+    if([CLLocationManager locationServicesEnabled]) {
+        lm.delegate = self;
+        lm.desiredAccuracy = kCLLocationAccuracyBest;
+        lm.distanceFilter = 100.0;
+        [lm startUpdatingLocation];
+
+        [_indicator1 startAnimating];
+        [_indicator2 startAnimating];
+    } else {
+        lm = NULL;
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
+{
+    CLLocation *newLocation = [locations lastObject];
+    _latitude.text = [[NSString alloc] initWithFormat:@"%g", newLocation.coordinate.latitude];
+    _longitude.text = [[NSString alloc] initWithFormat:@"%g", newLocation.coordinate.longitude];
+    [lm stopUpdatingLocation];
+
+    [_indicator1 stopAnimating];
+    [_indicator2 stopAnimating];
+}
+
+
+@end
